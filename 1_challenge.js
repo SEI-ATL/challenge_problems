40bd3dc02d86c548daa2d1e3de3873dd14358e5b
Nitish Dayal <contact@nitishdayal.me>
2020-10-27 06:18:46 -0700

    feat(challenge): Complete challenge

--- 1_challenge.js
+++ 1_challenge.js
@@ -3,7 +3,10 @@ Make a function createNewElement(tagName, text) that takes in 2
 parameters and returns a new element with the correct text
 ************************************************************** */
 function createNewElement(tagName, text) {
-    
+    const element = document.createElement(tagName)
+    element.textContent = text
+
+    return element
 }
 
 console.log(createNewElement('p', 'I am a software engineer'));
@@ -18,13 +21,16 @@ Make a function giveMeKeys(array) that takes in an array of objects
 and returns a new array with all of the keys from each object
 ************************************************************** */
 const instuctors = [
-    { rome: 'Rome', pete: 'Pete'},
-    { mike: 'Mike', dex: 'Dexter'},
+    { rome: 'Rome', pete: 'Pete' },
+    { mike: 'Mike', dex: 'Dexter' },
     { ga: 'General Assembly', cohort: 'SEI1019' }
 ]
 
 const giveMeKeys = (array) => {
-
+    return array.reduce((arr, obj) => {
+        arr.push(...Object.keys(obj))
+        return arr
+    }, [])
 }
 
 console.log(giveMeKeys(instuctors));
